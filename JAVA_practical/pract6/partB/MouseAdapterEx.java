6f30fcd161e1dc367a394d388e7a2a205ce5efca
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-07-27 08:54:31 +0530

    Added more

--- JAVA_practical/pract6/partB/MouseAdapterEx.java
+++ JAVA_practical/pract6/partB/MouseAdapterEx.java
@@ -15,8 +15,13 @@ public class MouseAdapterEx {
                 label.setText("Mouse Inside");
             }
             public void mouseExited(MouseEvent e){
-                
+                label.setText("Mouse Outside");
             }
         });
+
+        frame.add(label);
+        frame.setSize(300,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }    
 }
