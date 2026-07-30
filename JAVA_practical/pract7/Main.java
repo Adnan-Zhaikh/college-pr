00172691ad1cfaef0d250fcae934d471895d7883
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-07-30 09:24:56 +0530

    MODIFIED

--- JAVA_practical/pract7/Main.java
+++ JAVA_practical/pract7/Main.java
@@ -1,5 +1,5 @@
-package JAVA_practical.pract7;
 //Aim: Write a program to design a Calculator GUI and Perform arithmetic operations using Event Handling.
+package JAVA_practical.pract7;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,7 +10,7 @@ public class Main extends JFrame implements ActionListener{
     private JTextField  textField;
     private JButton[]  numberButtons;
     private JButton[] functionButtons;
-    private JButton[] addButton, subButton, mulButton, divButton, eqButton, clrButton, backspaceButton;
+    private JButton addButton, subButton, mulButton, divButton, eqButton, clrButton, backspaceButton;
     private JPanel panel, textFieldPanel;
 
     private double num1 = 0, num2 = 0, result = 0;
@@ -21,7 +21,52 @@ public class Main extends JFrame implements ActionListener{
         setSize(350,450);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
-        
+
+        textField = new JTextField();
+        textField.setEditable(false);
+        textField.setHorizontalAlignment(JTextField.RIGHT);
+        textField.setPreferredSize(new Dimension(300,50));
+        textField.setFont(new Font("Arial", Font.PLAIN, 40));
+
+        textFieldPanel = new JPanel(new BorderLayout());
+        textFieldPanel.add(textField, BorderLayout.CENTER);
+
+        numberButtons = new JButton[10];
+        for (int i = 0; i < 10; i++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(new Font("Arial", Font.PLAIN, 20));
+        }
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        eqButton = new JButton("=");
+        clrButton = new JButton("C");
+        backspaceButton = new JButton("<");
+
+        functionButtons = new JButton[](addButton, subButton, mulButton, divButton, eqButton, clrButton, backspaceButton)
+        for (JButton button : functionButtons) {
+            button.addActionListener(this);
+            button.setFont(new Font("Arial", Font.PLAIN, 20));
+        }
+        panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 4;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        panel.add(textFieldPanel, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+
+
+
+
     }
 
 }
